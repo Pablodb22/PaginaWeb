1754131251a313a23b9cf5e656a2598bef61b26f
Pablo Durán <149017383+Pablodb22@users.noreply.github.com>
2024-06-10 12:44:05 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,53 @@
+const baseDeDatos = [
+{
+    id: 1,
+    nombre:'camiseta1',
+    precio: 22,
+    img: 'Fotos/pexels-godisable-jacob-226636-970374.jpg'
+},
+{
+    id: 2,
+    nombre:'camiseta2',
+    precio: 20,
+    img: 'Fotos/pexels-kowalievska-1055691.jpg'
+},
+{
+    id: 3,
+    nombre:'camiseta3',
+    precio: 18,
+    img: 'Fotos/pexels-hazardos-1306246.jpg'
+},
+{
+    id: 4,
+    nombre:'pantalon1',
+    precio: 25,
+    img: 'Fotos/pexels-godisable-jacob-226636-885590.jpg'
+},
+{
+    id: 5,
+    nombre:'pantalon2',
+    precio: 28,
+    img: 'Fotos/pexels-cottonbro-4937449.jpg'
+},
+{
+    id: 6,
+    nombre:'pantalon3',
+    precio: 31,
+    img: 'Fotos/pexels-godisable-jacob-226636-1068638.jpg'
+},
+{
+    id: 7,
+    nombre:'zapatillas1',
+    precio: 29,
+    img: 'Fotos/pexels-moy-caro-photographer-296475578-21913838.jpg'
+},
+{
+    id: 8,
+    nombre:'zapatillas2',
+    precio: 29,
+    img: 'Fotos/pexels-wendelmoretti-1972115.jpg'
+}
+]
+
+
+
