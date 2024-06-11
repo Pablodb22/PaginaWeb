8dcf2237f8fbdb66bcd2271b27774a037e15d2b6
Pablo Durán <149017383+Pablodb22@users.noreply.github.com>
2024-06-11 12:01:04 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,53 +1,95 @@
 const baseDeDatos = [
 {
-    id: 1,
+    id: 'camiseta',
     nombre:'camiseta1',
     precio: 22,
+    descripcion: 'camiseta1',
     img: 'Fotos/pexels-godisable-jacob-226636-970374.jpg'
 },
 {
-    id: 2,
+    id: 'camiseta',
     nombre:'camiseta2',
+    descripcion:'camiseta2',
     precio: 20,
     img: 'Fotos/pexels-kowalievska-1055691.jpg'
 },
 {
-    id: 3,
+    id: 'camiseta',
     nombre:'camiseta3',
+    descripcion:'camiseta3',
     precio: 18,
     img: 'Fotos/pexels-hazardos-1306246.jpg'
 },
 {
-    id: 4,
+    id: 'pantalon',
     nombre:'pantalon1',
+    descripcion:'pantalon1',
     precio: 25,
     img: 'Fotos/pexels-godisable-jacob-226636-885590.jpg'
 },
 {
-    id: 5,
+    id: 'pantalon',
     nombre:'pantalon2',
-    precio: 28,
+    descripcion:'pantalon2',
+    precio: 21,
     img: 'Fotos/pexels-cottonbro-4937449.jpg'
 },
 {
-    id: 6,
+    id: 'pantalon',
     nombre:'pantalon3',
-    precio: 31,
+    descripcion:'pantalon3',
+    precio: 24,
     img: 'Fotos/pexels-godisable-jacob-226636-1068638.jpg'
 },
 {
-    id: 7,
+    id: 'zapatillas',
     nombre:'zapatillas1',
-    precio: 29,
+    descripcion:'zapatillas1',
+    precio: 32,
     img: 'Fotos/pexels-moy-caro-photographer-296475578-21913838.jpg'
 },
 {
-    id: 8,
+    id: 'zapatillas',
     nombre:'zapatillas2',
-    precio: 29,
+    descripcion:'zapatillas2',
+    precio: 35,
     img: 'Fotos/pexels-wendelmoretti-1972115.jpg'
 }
 ]
+
+const DOMitems = document.querySelector('#items');
+const botones=document.querySelectorAll('.boton-menu');
+
+function renderizarProductos(productoselegidos){
+    DOMitems.innerHTML="";
+    productoselegidos.forEach(producto => {
+       const div=document.createElement("div");
+       div.classList.add("product");
+       div.innerHTML =`
+        <img src="${producto.img}">
+        <h3 id=${producto.id}>${producto.nombre}</h3>
+        <p>${producto.descripcion}</p>
+        <p>${producto.precio}€</p>
+        <button>Añadir Carrito</button>
+        </div>`;
+        DOMitems.append(div);
+    })
+}
+
+
+botones.forEach(boton => {
+    boton.addEventListener("click",(e) => {   
+        if(e.currentTarget.id != "general"){
+        const productosBoton=baseDeDatos.filter(producto => producto.id===e.currentTarget.id);
+        renderizarProductos(productosBoton);
+        }else{
+            renderizarProductos(baseDeDatos);
+        }
+        
+    })
+});
+
+window.addEventListener("load",renderizarProductos(baseDeDatos));
 
 
 
